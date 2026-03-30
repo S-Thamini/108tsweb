76670d0fd36b2a4102684c271e15536575032da4
Estim <estim@example.com>
2026-03-30 14:45:49 +0530

    Deployment Final Build: Full Native Nix configuration (Bypassing PEP 668)

--- replit.nix
+++ replit.nix
@@ -2,5 +2,11 @@
   deps = [
     pkgs.python311Full
     pkgs.python311Packages.pip
+    pkgs.python311Packages.pandas
+    pkgs.python311Packages.fastapi
+    pkgs.python311Packages.uvicorn
+    pkgs.python311Packages.openpyxl
+    pkgs.python311Packages.requests
+    pkgs.python311Packages.python-multipart
   ];
 }
